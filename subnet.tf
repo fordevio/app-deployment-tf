8c0e0c9a44067345ee3858cf2f0435d72f7afc55
YASH PATEL <yp969803@gmail.com>
2024-08-01 14:08:47 +0530

    fix: private ip to subnet a

--- subnet.tf
+++ subnet.tf
@@ -3,6 +3,7 @@
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
   availability_zone = "ap-south-1a"
   tags = {
     Name = "public_a"
@@ -12,7 +13,6 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "private_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
   tags = {
     Name = "public_a"
